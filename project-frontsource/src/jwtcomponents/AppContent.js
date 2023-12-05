cbbcf87f666808bb4aa831d43691dbde9fa4e06a
Kamal <saikamal887@gmail.com>
2023-12-05 13:05:41 +0530

    first commit

---
+++ project-frontsource/src/jwtcomponents/AppContent.js
@@ -0,0 +1,76 @@
+import * as React from "react";
+
+import { request, setAuthHeader } from "../helpers/axios_helper";
+
+import Buttons from "./Buttons";
+import AuthContent from "./AuthContent";
+import LoginForm from "./LoginForm";
+import WelcomeContent from "./WelcomeContent";
+
+export default class AppContent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      componentToShow: "welcome",
+    };
+  }
+
+  login = () => {
+    this.setState({ componentToShow: "login" });
+  };
+
+  logout = () => {
+    this.setState({ componentToShow: "welcome" });
+    setAuthHeader(null);
+  };
+
+  onLogin = (e, username, email, password) => {
+    e.preventDefault();
+    request("POST", "/login", {
+      login: username,
+      email: email,
+      password: password,
+    })
+      .then((response) => {
+        setAuthHeader(response.data.token);
+        this.setState({ componentToShow: "messages" });
+      })
+      .catch((error) => {
+        setAuthHeader(null);
+        this.setState({ componentToShow: "welcome" });
+      });
+  };
+
+  onRegister = (event, firstName, lastName, email, username, password) => {
+    event.preventDefault();
+    request("POST", "/register", {
+      firstName: firstName,
+      lastName: lastName,
+      login: username,
+      email: email,
+      password: password,
+    })
+      .then((response) => {
+        setAuthHeader(response.data.token);
+        this.setState({ componentToShow: "messages" });
+      })
+      .catch((error) => {
+        setAuthHeader(null);
+        this.setState({ componentToShow: "welcome" });
+      });
+  };
+
+  render() {
+    return (
+      <>
+        <Buttons login={this.login} logout={this.logout} />
+
+        {this.state.componentToShow === "welcome" && <WelcomeContent />}
+        {this.state.componentToShow === "login" && (
+          <LoginForm onLogin={this.onLogin} onRegister={this.onRegister} />
+        )}
+        {this.state.componentToShow === "messages" && <AuthContent />}
+      </>
+    );
+  }
+}
